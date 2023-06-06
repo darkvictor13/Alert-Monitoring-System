5d2b001f5a7fcd2c03163135c378e88913d56077
Victor Emanuel Almeida <51101723+darkvictor13@users.noreply.github.com>
2023-06-05 21:30:44 -0300

    creating devices

--- frontend/components/device/deviceList.tsx
+++ frontend/components/device/deviceList.tsx
@@ -1,5 +1,7 @@
 import {
   Box,
+  Button,
+  Modal,
   Paper,
   Table,
   TableBody,
@@ -8,6 +10,7 @@ import {
   TableHead,
   TablePagination,
   TableRow,
+  TextField,
   useTheme,
 } from "@mui/material";
 import { NextPage } from "next";
@@ -17,24 +20,80 @@ import backendApi from "../../lib/axios/backend_api";
 import StickyFooter from "../stickyFooter";
 import TopTab from "../topTab/topTab";
 import { formatDistanceToNow } from "date-fns";
+import { Add, Send } from "@mui/icons-material";
+import { useAuth } from "../../hooks/useAuth";
+import { useSWRClient } from "../../hooks/useSwrClient";
 
 const DeviceList: NextPage<{ userId: number }> = ({ userId }) => {
+  // show modal for creating device
+  const [showCreateDevice, setShowCreateDevice] = useState(false);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [devices, setDevices] = useState<IDevice[]>();
+  //const [devices, setDevices] = useState<IDevice[]>();
   const theme = useTheme();
 
-  useEffect(() => {
-    console.log("fetching devices");
-    function fetchDevices() {
-      backendApi.get<IDevice[]>(`/device/user/${userId}`).then((response) => {
-        if (response.data) {
-          setDevices(response.data);
-        }
-      });
-    }
-    fetchDevices();
-  }, [userId, setDevices]);
+  // useSWR to get the devices
+  const { data, isLoading, mutateAndRefetch } = useSWRClient<IDevice[]>(
+    `/device/user/${userId}`
+  );
+
+  const CreateDevice = () => {
+    const { isAuth, id } = useAuth();
+    const handleCreateDevice = async (
+      event: React.FormEvent<HTMLFormElement>
+    ) => {
+      event.preventDefault();
+      const form = new FormData(event.currentTarget);
+      const name = form.get("name") as string;
+      try {
+        await backendApi.post("/device", { name, userId: id });
+        setShowCreateDevice(false);
+        mutateAndRefetch();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    return (
+      <Box
+        sx={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "50dvw",
+          bgcolor: "background.paper",
+          border: "2px solid #000",
+          boxShadow: 24,
+          p: 4,
+        }}
+      >
+        <Box
+          component="form"
+          onSubmit={handleCreateDevice}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="name"
+            label="Device name"
+            type="text"
+            id="name"
+            autoComplete="current-device-name"
+          />
+          <Button startIcon={<Send />} variant="contained" type="submit">
+            Create
+          </Button>
+        </Box>
+      </Box>
+    );
+  };
 
   return (
     <Box
@@ -45,10 +104,23 @@ const DeviceList: NextPage<{ userId: number }> = ({ userId }) => {
       }}
     >
       <TopTab />
+      <Button
+        onClick={() => {
+          setShowCreateDevice(true);
+        }}
+        variant="contained"
+        startIcon={<Add />}
+        sx={{ margin: "20px", width: "fit-content", alignSelf: "flex-end" }}
+      >
+        Create Device
+      </Button>
+      <Modal open={showCreateDevice} onClose={() => setShowCreateDevice(false)}>
+        <CreateDevice />
+      </Modal>
       <Paper
         sx={{
           flexGrow: 1,
-          margin: "20px",
+          margin: "0px 20px 20px 20px",
           width: "auto",
           border: `1px solid ${theme.palette.primary.main}`,
           borderRadius: "16px",
@@ -86,7 +158,7 @@ const DeviceList: NextPage<{ userId: number }> = ({ userId }) => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {devices
+              {data
                 ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((device) => (
                   <TableRow key={device.uuid}>
@@ -111,7 +183,7 @@ const DeviceList: NextPage<{ userId: number }> = ({ userId }) => {
           rowsPerPage={rowsPerPage}
           rowsPerPageOptions={[1, 10, 25, 100]}
           component="div"
-          count={devices?.length || 0}
+          count={data?.length || 0}
           page={page}
           onPageChange={(event, page) => {
             setPage(page);
